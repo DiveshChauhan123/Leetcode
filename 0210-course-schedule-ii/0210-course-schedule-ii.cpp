21f3238ff0e7c9cdb76f8027738bcac693ce5995
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2024-09-24 19:38:49 +0530

    Time: 16 ms (55.77%), Space: 18.8 MB (16.32%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=prerequisites.size();
+        unordered_map<int,list<int>>mp;
+        unordered_map<int,int>indegree;
+        for(int i=0;i<n;i++){
+            int x=prerequisites[i][0];
+            int y=prerequisites[i][1];
+            mp[y].push_back(x);
+            indegree[x]++;
+        }
+        vector<int>ans;
+        queue<int>q;
+        int count=0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+                count++;
+            }
+        }
+        while(!q.empty()){
+            int x=q.front();
+            ans.push_back(x);
+            q.pop();
+            for(auto neigh:mp[x]){
+                indegree[neigh]--;
+                if(indegree[neigh]==0){
+                    q.push(neigh);
+                    count++;
+                }
+            }
+        }
+        if(count==numCourses){
+            return ans;
+        }
+        ans.clear();
+        return ans;
+    }
+};
