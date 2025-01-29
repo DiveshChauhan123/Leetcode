d586ddb1bba8e187c885d282e41fec6505e87ac0
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-01-29 16:56:38 +0530

    Time: 476 ms (23.81%), Space: 41.7 MB (33.73%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    int n;
+    int dp[2501][2501]; // for memoization 
+    int solve(vector<int>& nums, int i, int p){
+        if(i >= n) return 0;
+
+        // check it is already evaluated or not in memoization
+        if(p!=-1 && dp[i][p] != -1) return dp[i][p];
+
+        // when take it index value
+        int take = 0;
+        // check those value is greater than the previous stored value 
+        if(p==-1 || nums[i] > nums[p]){
+            take = 1 + solve(nums, i+1, i);
+        }
+
+        // not take that value skip
+        int skip = solve(nums, i+1, p);
+        
+        // when p = -1 then do not try to store in memoize array or access it
+        if(p != -1) dp[i][p] = max(take, skip);
+
+        return max(take, skip);
+    }
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(nums.size(),1);
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<i;j++){
-                int skip=dp[i];
-                int take=0;
-                if(nums[i]>nums[j]){
-                    take=dp[j]+1;
-                }
-                dp[i]=max(skip,take);
-            }
-        }
-        int l=0;
-        for(int i=0;i<n;i++){
-            l=max(l,dp[i]);
-        }
-        return l;
+        n = nums.size();
+        // fill memoize array value by default -1
+        memset(dp, -1, sizeof(dp));
+        return solve(nums, 0, -1);
     }
 };
