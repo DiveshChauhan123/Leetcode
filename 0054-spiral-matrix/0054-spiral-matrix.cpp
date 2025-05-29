c743946db1e00379e290e1761945314cd33ce0b8
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-05-29 15:38:05 +0530

    Time: 0 ms (100%), Space: 9.4 MB (49.23%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-    vector<vector<bool>> visited(m, vector<bool>(n, false));
+    int m = matrix.size(), n = matrix[0].size();
     vector<int> result;
-    
-    // Directions: right, down, left, up
-    int dr[] = {0, 1, 0, -1};
-    int dc[] = {1, 0, -1, 0};
 
-    int r = 0, c = 0, d = 0;
-    for (int i = 0; i < m * n; ++i) {
-        result.push_back(matrix[r][c]);
-        visited[r][c] = true;
-        int nr = r + dr[d], nc = c + dc[d];
-        if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
-            r = nr;
-            c = nc;
-        } else {
-            d = (d + 1) % 4;
-            r += dr[d];
-            c += dc[d];
+    int top = 0, bottom = m - 1;
+    int left = 0, right = n - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse from Left to Right
+        for (int j = left; j <= right; ++j)
+            result.push_back(matrix[top][j]);
+        top++;
+
+        // Traverse from Top to Bottom
+        for (int i = top; i <= bottom; ++i)
+            result.push_back(matrix[i][right]);
+        right--;
+
+        // Traverse from Right to Left
+        if (top <= bottom) {
+            for (int j = right; j >= left; --j)
+                result.push_back(matrix[bottom][j]);
+            bottom--;
+        }
+
+        // Traverse from Bottom to Top
+        if (left <= right) {
+            for (int i = bottom; i >= top; --i)
+                result.push_back(matrix[i][left]);
+            left++;
         }
     }
+
     return result;
     }
 };
