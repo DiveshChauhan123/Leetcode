178aff76ad83ee7acccf9cda42b94a80b373e4cb
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-06-19 12:02:53 +0530

    Time: 37 ms (64.95%), Space: 86.4 MB (98.52%) - LeetHub

--- 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        int count=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)mp[nums[i]]++;
-        for(int i=n-1;i>0;i--){
-            if(nums[i]==nums[i-1])continue;
-            count+=mp[nums[i]];
-            mp[nums[i-1]]+=mp[nums[i]];
+        sort(nums.begin(), nums.end());
+        int count = 0, ops = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                count++;
+            }
+            ops += count;
         }
-        return count;
+
+        return ops;
     }
 };
