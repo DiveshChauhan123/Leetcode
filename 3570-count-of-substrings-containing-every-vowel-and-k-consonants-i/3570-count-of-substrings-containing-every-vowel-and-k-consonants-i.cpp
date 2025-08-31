a9ef91544934506cec15ec87a392c5a2d097b7e8
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-08-31 12:48:16 +0530

    Time: 2 ms (88.86%), Space: 10.2 MB (76.88%) - LeetHub

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool isVowel(char c) {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
+    }
+
+    int countOfSubstrings(string word, int k) {
+        int n=word.size();
+        unordered_map<char,int>mp;
+        int lastindex=n;
+        vector<int>nextcons(n);
+        for(int i=n-1;i>=0;i--){
+            nextcons[i]=lastindex;
+            if(!isVowel(word[i])){
+                lastindex=i;
+            }
+        }
+        long long count=0;
+        int consonants=0;
+        int i=0,j=0;
+        while(j<n){
+            if(isVowel(word[j]))mp[word[j]]++;
+            else{
+                consonants++;
+            }
+            while(consonants>k){
+                if(isVowel(word[i])){
+                    mp[word[i]]--;
+                    if(mp[word[i]]==0){
+                        mp.erase(word[i]);
+                    }
+                }
+                else{
+                    consonants--;
+                }
+                i++;
+            }
+            while(mp.size()==5 && consonants==k){
+                int idx=nextcons[j];
+                count+=(idx-j);
+                if(isVowel(word[i])){
+                    mp[word[i]]--;
+                    if(mp[word[i]]==0){
+                        mp.erase(word[i]);
+                    }
+                }
+                else{
+                    consonants--;
+                }
+                i++;
+            }
+            j++;
+        }
+        return count;
+    }
+};
