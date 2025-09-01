df1658f054905bbd40aabc7a2a65696581ff9519
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-09-01 09:55:17 +0530

    Time: 102 ms (49.52%), Space: 85.1 MB (48.39%) - LeetHub

---
+++ 2678-design-graph-with-shortest-path-calculator/2678-design-graph-with-shortest-path-calculator.cpp
@@ -0,0 +1,45 @@
+class Graph {
+public:
+unordered_map<int,vector<pair<int,int>>>mp;
+int nodes;
+    Graph(int n, vector<vector<int>>& edges) {
+        nodes=n;
+        for(int i=0;i<edges.size();i++){
+            mp[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+        }
+    }
+    
+    void addEdge(vector<int> edge) {
+        mp[edge[0]].push_back({edge[1],edge[2]});
+    }
+    
+    int shortestPath(int node1, int node2) {
+        vector<int> dist(nodes+1, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        dist[node1] = 0;
+        pq.push({0,node1});
+        while(!pq.empty()){
+            int distance=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+            if(distance>dist[node])continue;
+            for(auto i:mp[node]){
+                int neigh=i.first;
+                int value=i.second;
+                if(dist[node]+value<dist[neigh]){
+                    dist[neigh]=dist[node]+value;
+                    pq.push({dist[neigh],neigh});
+                }
+            }
+        }
+        return dist[node2]==INT_MAX?-1:dist[node2];
+    }
+};
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph* obj = new Graph(n, edges);
+ * obj->addEdge(edge);
+ * int param_2 = obj->shortestPath(node1,node2);
+ */
