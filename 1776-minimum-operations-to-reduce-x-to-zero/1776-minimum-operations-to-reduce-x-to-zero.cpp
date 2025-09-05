a781027311c3a99f251b2c90de549c749a92bc8f
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-09-05 15:48:52 +0530

    Time: 8 ms (38.95%), Space: 102.4 MB (39.64%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -4,12 +4,17 @@ public:
         int sum=accumulate(nums.begin(),nums.end(),0);
         int curr=0;
         int maxLen=INT_MIN;
-        for(int l=0,r=0;r<nums.size();r++){
+        int l=0,r=0;
+        while(r<nums.size()){
             curr+=nums[r];
-            while(l<=r && curr>sum-x)curr-=nums[l++];
+            while(l<=r && curr>sum-x){
+                curr-=nums[l];
+                l++;
+            }
             if(curr==sum-x){
                 maxLen=max(maxLen,r-l+1);
             }
+            r++;
         }
         return maxLen==INT_MIN?-1:nums.size()-maxLen;
     }
