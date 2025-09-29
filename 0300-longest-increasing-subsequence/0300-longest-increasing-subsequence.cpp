73ee7e690ddde321b4b628f9398d8ebc3d864c03
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-09-30 01:28:11 +0530

    Time: 319 ms (30.81%), Space: 320.9 MB (6.71%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -16,8 +16,24 @@ public:
         return t[i][prevIdx + 1] = max(take, notTake);
     }
 
+    
     int lengthOfLIS(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-        return solve(0, nums, -1);
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // dp[i][prevIdx+1] = LIS length starting from i with prevIdx
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int prevIdx = i - 1; prevIdx >= -1; prevIdx--) {
+                int notTake = dp[i + 1][prevIdx + 1];
+                int take = 0;
+                if (prevIdx == -1 || nums[i] > nums[prevIdx]) {
+                    take = 1 + dp[i + 1][i + 1];  
+                }
+                dp[i][prevIdx + 1] = max(take, notTake);
+            }
+        }
+        return dp[0][0]; // i=0, prevIdx=-1 -> stored at [0][0]
     }
+
+
 };
