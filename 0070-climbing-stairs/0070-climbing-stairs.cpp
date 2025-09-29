ebd0f7b3b75e71f7652c3838105615d392c0148a
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-09-29 22:02:45 +0530

    Time: 0 ms (100%), Space: 7.9 MB (58.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,28 @@
 class Solution {
+// int t[46];
+// int solve(int n){
+//     if(n<0)return 0;
+//     if(n==0)return 1;
+//     if(t[n]!=-1)return t[n];
+//     int take=solve(n-1);
+//     int nottake=solve(n-2);
+//     return t[n]=nottake+take;
+// }
+//     int climbStairs(int n) {
+//         memset(t,-1,sizeof(t));
+//         return solve(n);
+//     }
+// };
 public:
 int t[46];
-int solve(int n){
-    if(n<0)return 0;
-    if(n==0)return 1;
-    if(t[n]!=-1)return t[n];
-    int take=solve(n-1);
-    int nottake=solve(n-2);
-    return t[n]=nottake+take;
-}
     int climbStairs(int n) {
-        memset(t,-1,sizeof(t));
-        return solve(n);
+        t[0]=1;
+        for(int i=1;i<=n;i++){
+            t[i]+=t[i-1];
+            if(i-2>=0){
+                t[i]+=t[i-2];
+            }
+        }
+        return t[n];
     }
 };
