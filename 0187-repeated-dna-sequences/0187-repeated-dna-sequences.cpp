46e2a3c6e0dbe0f4d96609f8af0b6494536079f7
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2025-12-21 18:20:40 +0530

    Time: 35 ms (82.94%), Space: 22.2 MB (80.32%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        unordered_set<string>seen,repeated;
         vector<string>result;
-        set<string>t,ans;
         int i=0,j=0;
         while(j<s.size()){
-            if(j-i+1==10){
+            if(j-i+1==10 ){
                 string p=s.substr(i,10);
-                if(t.find(p)!=t.end() ){
-                    ans.insert(p);
-                }
-                else{
-                    t.insert(p);
+                if(!seen.insert(p).second){
+                    repeated.insert(p);
                 }
                 i++;
             }
             j++;
         }
-        for(auto i:ans)result.push_back(i);
+        for(auto i:repeated)result.push_back(i);
         return result;
     }
 };
