6c5f9b3933f6b7d49b387998d6b024a4764bf82b
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2026-03-28 18:56:11 +0530

    Time: 0 ms (100%), Space: 31.3 MB (67.05%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-void rotate(vector<vector<int>>& nums, int k,int i) {
-    int m = nums.size();
-    int n = nums[0].size();
-
-    k %= n;
-
-    
-
-        vector<int> temp(n);
-
-        if (i % 2 == 0) {  
-            // even row → left rotate
-            for (int j = 0; j < n; j++) {
-                temp[j] =nums[i][(j + k) % n];
-            }
-        } 
-        else {  
-            // odd row → right rotate
-            for (int j = 0; j < n; j++) {
-                temp[(j + k) % n] = nums[i][j];
-            }
-        }
-
-        nums[i] = temp;
-    
-}
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        vector<vector<int>>nums=mat;
-        for(int i=0;i<mat.size();i++){
-            rotate(nums,k,i);
+
+        int m = mat.size();
+        int n = mat[0].size();
+
+        k %= n;
+
+        for(int i = 0; i < m; i++) {
+
+            for(int j = 0; j < n; j++) {
+
+                if(i % 2 == 0) {  
+                    // even row → left rotation
+                    if(mat[i][j] != mat[i][(j + k) % n])
+                        return false;
+                }
+                else {  
+                    // odd row → right rotation
+                    if(mat[i][j] != mat[i][(j - k + n) % n])
+                        return false;
+                }
+            }
         }
-        if(nums==mat)return true;
-        return false;
+
+        return true;
     }
 };
