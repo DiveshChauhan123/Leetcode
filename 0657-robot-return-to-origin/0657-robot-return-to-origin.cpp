ce607aaddb25a979dba0c5b2ce2fc1ff524cc03b
divesh singh chauhan <123537228+DiveshChauhan123@users.noreply.github.com>
2026-04-05 17:56:44 +0530

    Time: 4 ms (27.83%), Space: 10.3 MB (47.26%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n=moves.size();
+        int x=0,y=0;
+        for(int i=0;i<n;i++){
+            if(moves[i]=='U')x--;
+            else if(moves[i]=='D')x++;
+            else if(moves[i]=='L')y--;
+            else{
+                y++;
+            }
+        }
+        if(x==0 && y==0)return true;
+        return false;
+    }
+};
